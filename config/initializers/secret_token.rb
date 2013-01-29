f45f220ebef0d1d89ce629ae211e050a5e9a02a2
U,S <uetashunya@Uetas-MacBook-Pro.local>
2013-01-29 19:00:35 +0900

    CareerSupportCenter
    名前を分かりやすく変更

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CareerSupprtCenter::Application.config.secret_token = 'f9b27426cef5e7d68b3978548752dbcb0f15a6f8cb445e4488c0f23e0db766716c4144653bd2c7df4bbdcf7d83e17cfcb7d770838043788669d0245e005a730a'
